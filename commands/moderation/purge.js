446966601d35869001ae23357fb10dfdea385d4d
Juan <juampigarcia2014@gmail.com>
2020-05-29 03:30:34 -0300

    Fix purge command

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -5,15 +5,16 @@ exports.run = (client, message, args) => {
     if (!amount) return message.reply('debes especificar una cantidad!');
     if (!amount && !user) return message.reply('debes especificar usuario y cantidad, o solo cantidad, de mensajes a eliminar!');
 
-    message.channel.messages.fetch({
-        limit: 100,
-       }).then((messages) => {
-        if (user) {
-        const filterBy = user ? user.id : client.user.id;
-        messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
-        }
-        message.channel.bulkDelete(messages).catch(error => client.logger.log(error.stack));
-       });
+    message.delete();
+
+    message.channel.messages.fetch({ limit: amount })
+        .then((messages) => {
+            if (user) {
+                const filterBy = user ? user.id : client.user.id;
+                messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+            }
+            message.channel.bulkDelete(messages).catch(error => client.logger.log(error.stack, "error"));
+        });
 };
 
 exports.help = {
