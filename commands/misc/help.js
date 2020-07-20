83b753a9ff15e528f3e58f0cf59fafc66f358592
Juan <juampigarcia2014@gmail.com>
2020-07-20 00:30:44 -0300

    Delete function toCamelCase()

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -1,12 +1,5 @@
 const { MessageEmbed } = require("discord.js");
 
-String.prototype.toCamelCase = function (str) {
-    return str
-        .replace(/\s(.)/g, function ($1) { return $1.toUpperCase(); })
-        .replace(/\s/g, '')
-        .replace(/^(.)/, function ($1) { return $1.toLowerCase(); });
-}
-
 exports.run = (client, message, args) => {
     const embed = new MessageEmbed().setColor("BLUE");
     let data = [];
