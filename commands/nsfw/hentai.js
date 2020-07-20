e93f6de9996d72514cd1d6c732ce33f3c953f1a7
Juan <juampigarcia2014@gmail.com>
2020-07-20 00:30:07 -0300

    Improve nsfw commands

--- commands/nsfw/hentai.js
+++ commands/nsfw/hentai.js
@@ -3,57 +3,15 @@ const client = require('nekos.life');
 const { sfw, nsfw } = new client();
 
 exports.run = async (client, message, args) => {
-    const nsfwChannel = message.channel.nsfw ? "nsfw" : "sfw";
-
-    const msg = await message.channel.send("Generando...");
-
-    const functionArray = [
-        nsfw.randomHentaiGif(),
-        nsfw.pussy(),
-        nsfw.lesbian(),
-        nsfw.kuni(),
-        nsfw.cumsluts(),
-        nsfw.classic(),
-        nsfw.boobs(),
-        nsfw.bJ(),
-        nsfw.anal(),
-        nsfw.yuri(),
-        nsfw.tits(),
-        nsfw.girlSoloGif(),
-        nsfw.girlSolo(),
-        nsfw.pussyWankGif(),
-        nsfw.pussyArt(),
-        nsfw.kemonomimi(),
-        nsfw.kitsune(),
-        nsfw.keta(),
-        nsfw.holo(),
-        nsfw.holoEro(),
-        nsfw.hentai(),
-        nsfw.feetGif(),
-        nsfw.eroFeet(),
-        nsfw.feet(),
-        nsfw.ero(),
-        nsfw.eroKitsune(),
-        nsfw.eroKemonomimi(),
-        nsfw.eroNeko(),
-        nsfw.eroYuri(),
-        nsfw.cumArts(),
-        nsfw.blowJob()
-    ];
-
-    const embed = new MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle((await sfw.catText()).cat);
-
-    if (nsfwChannel === "nsfw") {
-        embed.setImage((await (functionArray[Math.floor(Math.random() * functionArray.length)])).url);
+    if (message.channel.nsfw) {
+        const embed = new MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle((await sfw.catText()).cat)
+            .setImage((await nsfw.randomHentaiGif()).url);
+        message.channel.send(embed)
     } else {
-        msg.delete();
-        return message.channel.send("El canal no es \`NSFW\`.");
+        message.channel.send("El canal no es \`NSFW\`");
     }
-
-    message.channel.send(embed);
-    msg.delete();
 };
 
 exports.help = {
--- commands/nsfw/neko.js
+++ commands/nsfw/neko.js
@@ -1,20 +1,17 @@
 const { MessageEmbed } = require("discord.js");
 const client = require('nekos.life');
-const { nsfw } = new client();
+const { sfw, nsfw } = new client();
+
 
 exports.run = async (client, message, args) => {
     if (message.channel.nsfw) {
-        const msg = await message.channel.send("Generando...");
-        const embed = new MessageEmbed()
-            .setColor("BLUE")
-            .setTitle((await sfw.catText()).cat);
-
-        embed.setImage((await nsfw.neko()).url);
+        const embed = new MessageEmbed().setColor("BLUE")
+            .setTitle((await sfw.catText()).cat)
+            .setImage((await nsfw.neko()).url);;
         message.channel.send(embed);
-        msg.delete();
     } else {
         message.channel.send(`El canal no es \`NSFW\``);
-    }
+    };
 };
 
 exports.help = {
