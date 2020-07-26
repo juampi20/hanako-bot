e280bbdc4c6de3c49e0edbb7aeb054a33499d8d6
Juan <juampigarcia2014@gmail.com>
2020-07-26 19:37:00 -0300

    Heroku Changes

--- Procfile
+++ Procfile
@@ -1,1 +1,5 @@
-worker: node ./src/index.js
+# I don't wanna be a web process
+web: echo "I don't want a web process"
+
+# I wanna be a service process
+service: npm start 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const Discord = require("discord.js");
-const fs = require("fs");
+const Discord = require('discord.js');
+const fs = require('fs');
 const path = require('path');
-//const SQLite = require("better-sqlite3");
+//const SQLite = require('better-sqlite3');
 
 const client = new Discord.Client();
 
-client.config = require("./config.js");
-client.logger = require("./logger.js");
+client.config = require('./config.js');
+client.logger = require('./logger.js');
 client.commands = new Discord.Collection();
 //client.sql = new SQLite('./data/scores.sqlite');
 
@@ -18,13 +18,13 @@ client.commands = new Discord.Collection();
 
 // Events Handler
 async function eventsHandler(client, dir) {
-    fs.readdir(path.join(__dirname,'events'), (err, files) => {
-        if (err) return client.logger.log(err, "error");
+    fs.readdir(path.join(__dirname, dir), (err, files) => {
+        if (err) return client.logger.log(err, 'error');
         client.logger.log(`Cargando un total de ${files.length} eventos.`);
         files.forEach(file => {
             // const event = require(`./events/${file}`);
-            let event = require(path.join(__dirname,'events',file));
-            let eventName = file.split(".")[0];
+            let event = require(path.join(__dirname, dir, file));
+            let eventName = file.split('.')[0];
             client.on(eventName, event.bind(null, client));
         });
     });
@@ -33,16 +33,16 @@ async function eventsHandler(client, dir) {
 // Commands Handlers
 async function commandsHandler(client, dir) {
     fs.readdir(path.join(__dirname, dir), (err, folders) => {
-        if (err) return client.logger.log(err, "error");
+        if (err) return client.logger.log(err, 'error');
         folders.forEach(folder => {
             fs.readdir(path.join(__dirname, dir, folder), (err, files) => {
-                if (err) return client.logger.log(err, "error");
+                if (err) return client.logger.log(err, 'error');
                 client.logger.log(`Cargando un total de ${files.length} comandos (${folder}).`);
                 files.forEach(file => {
-                    if (!file.endsWith(".js")) return;
+                    if (!file.endsWith('.js')) return;
                     // let props = require(`./commands/${folder}/${file}`);
                     let props = require(path.join(__dirname, dir, folder, file));
-                    let commandName = file.split(".")[0];
+                    let commandName = file.split('.')[0];
                     client.commands.set(commandName, props);
                 });
             });
