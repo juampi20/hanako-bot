221857acdbb27850ffb827f02cfe24e0068c0eec
Juan Pablo <57530802+juampi20@users.noreply.github.com>
2020-09-16 01:11:45 -0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,18 @@
 module.exports = (client) => {
+    const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
+    if (!table['count(*)']) {
+      // If the table isn't there, create it and setup the database correctly.
+      sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER);").run();
+      // Ensure that the "id" row is always unique and indexed.
+      sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
+      sql.pragma("synchronous = 1");
+      sql.pragma("journal_mode = wal");
+    }
+
+    // And then we have two prepared statements to get and set the score data.
+    client.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
+    client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
+    
     client.logger.log(`${client.user.tag} esta listo.`, "ready");
     client.user.setActivity("Made with ❤");
 };
